ccac91dd453ddfbfb4ed3b40b2f983d6ed74fa09
wy <wy@example.com>
2016-10-06 10:53:40 +0800

    改变文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,1 @@
-public class HelloWorld{public static void main(String[] args){System.out.println(你好);}}
+public class HelloWorld{public static void main(String[] args){System.out.println(你好);test()}public void test();public void play();}
