f46f161b4f3d0c634302a1d698fdd010b9db8409
cherryblosssom <2990706720@qq.com>
2016-10-06 12:02:59 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,1 @@
-public class HelloWorld{public static void main(String[] args){System.out.println(你好);test()}public void test();public void play();}
+public class HelloWorld{public static void main(String[] args){System.out.println(你好);System.out.println("欢迎使用github");test();}public void test();public void play();}
